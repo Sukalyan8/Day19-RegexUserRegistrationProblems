1798267470cd29a9057793f6fabe923ecbfb21a0
Sukalyan <sukalyandash1920@gmail.com>
2022-01-25 21:53:24 +0530

    ReGex_UserRegistration

---
+++ ReGex_UserRegistration/UC8_Password_Rule-04.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReGex_UserRegistration
+{
+    public class UC8_Password_Rule_04
+    {
+        public string Regex_Pattern = "^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Pattern);
+        }
+    }
+}
