f9189c8b09420ef98686e509994dbcbbece7fcaf
Sukalyan <sukalyandash1920@gmail.com>
2022-01-25 22:10:12 +0530

    Enter Pre Defined Password

---
+++ UC5_EnterPreDefinedPassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReGex_UserRegistration
+{
+    public class UC5_EnterPreDefinedPassword
+    {
+        public string Regex_Pattern = @"[a-z,A-Z,0-9]{8,}$";
+
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Pattern);
+        }
+    }
+}
