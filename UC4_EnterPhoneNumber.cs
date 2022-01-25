545a56eafefc317c936928dba9886591eab31ba9
Sukalyan <sukalyandash1920@gmail.com>
2022-01-25 22:08:53 +0530

    Enter Phone Number

---
+++ UC4_EnterPhoneNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReGex_UserRegistration
+{
+    public class UC4_EnterPhoneNumber
+    {
+        public string Regex_Pattern = "^[/+]{1}[9]{1}[1]{1}[7-9]{1}[0-9]{9}$";
+
+        public bool validatePhoneNumber(string PhoneNumber)
+        {
+            return Regex.IsMatch(PhoneNumber, Regex_Pattern);
+        }
+    }
+}
